78175923235af82c49d61371b82a75a56079e780
Cameron Radmore <radmorecameron@gmail.com>
2026-09-13 11:21:56 -0400

    switch to warn

--- src/renderer/store/modules/settings.js
+++ src/renderer/store/modules/settings.js
@@ -329,7 +329,7 @@ const sideEffectHandlers = {
     let targetLocale = value
     if (value === 'system') {
       const systemLocaleName = (await getSystemLocale()).replace('_', '-') // ex: en-US
-      console.log(systemLocaleName)
+      console.warn(systemLocaleName)
       const systemLocaleSplit = systemLocaleName.split('-') // ex: en
       const targetLocaleOptions = allLocales.filter((locale) => {
         // filter out other languages
